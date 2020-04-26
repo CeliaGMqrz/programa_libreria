95861d5f66a5364e71b083c6d67a96758ac2a574
Celia Garcia <cg.marquez95@gmail.com>
2020-04-27 00:28:42 +0200

    Provocar error en la app

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def info_libros(isbn):
 
 # Definimos la tercera ruta de las categorías.
 @app.route('/categoria/<categoria>',methods=["GET","POST"])
-def categoria(categoria):
+def categoria(categ):
     for category in info:
         if "categories" in category.keys() and categoria in category["categories"]:
             return render_template("categoria.html", lista_libros=info, categoria=categoria)
